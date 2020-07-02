877106d8863703322d2a54c027fb8146ea4290af
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2020-07-03 01:04:25 +0200

    Update script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mv /opt/hostedtoolcache/Ruby/2.7.1/x64/bin/asciidoctor /opt/hostedtoolcache/Ruby/2.7.1/x64/bin/asciidoctor_real
-mv asciidoctor /opt/hostedtoolcache/Ruby/2.7.1/x64/bin/
+mv ./scripts/asciidoctor /opt/hostedtoolcache/Ruby/2.7.1/x64/bin/
